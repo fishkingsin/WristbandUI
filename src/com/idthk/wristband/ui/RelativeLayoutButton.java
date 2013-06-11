e8e6f2867f6c82235a21b3b77300eeeb2f21c15c
James Kong <fishkingsin@gmail.com>
2013-06-11 10:46:48 +0800

    update class

--- src/com/idthk/wristband/ui/RelativeLayoutButton.java
+++ src/com/idthk/wristband/ui/RelativeLayoutButton.java
@@ -15,7 +15,7 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class RelativeLayoutButton extends RelativeLayout {
-
+	static final String TAG="RelativeLayoutButton";
 	public RelativeLayoutButton(Context context) {
 		this(context, null);
 	}
@@ -30,65 +30,43 @@ public class RelativeLayoutButton extends RelativeLayout {
 
 		TypedArray a = context.obtainStyledAttributes(attrs,
 				R.styleable.RelativeLayoutButton, 0, 0);
-//		String titleText = a
-//				.getString(R.styleable.RelativeLayoutButton_titleText);
-//		int valueColor = a.getColor(
-//				R.styleable.RelativeLayoutButton_valueColor,
-//				android.R.color.holo_blue_light);
 		a.recycle();
-		LayoutInflater inflater = (LayoutIntabflater) context
-				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		inflater.inflate(R.layout.setting_tab, this, true);
+		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View view = inflater.inflate(R.layout.setting_tab, this, true);
 		
-		
-		    
-//		Log.v("RelativeLayoutButton" , "construct");
-//		TextView title = (TextView)inflater.inflate(R.id.setting_tab_title_textview, this);
-//		if(title!=null)
-//		{
-//			title.setText("title");
-//		}
-//		TextView title2 = (TextView)inflater.inflate(R.id.setting_tab_goal_textview, this);
-//		if(title2!=null)
-//		{
-//			title2.setText("value");
-//		}
-//		TextView title = (TextView) inflater.inflate(R.id.setting_tab_goal_textview,this);
-//	    title.setText(titleText);
-//
-//	    
-//	    TextView title2 = (TextView) getChildAt(1);
-//	    title2.setText(titleText);
-
+		TextView tv1 = (TextView)view.findViewById(R.id.setting_tab_title_textview);
+		TextView tv2 = (TextView)view.findViewById(R.id.setting_tab_goal_textview);
+		tv1.setText("TV1");
+		tv2.setText("TV2");
 	}
 
 	// method for setting texts for the text views
-//	public void setText(int id, CharSequence text) {
-//		View v = findViewById(id);
-//		if (null != v && v instanceof TextView) {
-//			((TextView) v).setText(text);
-//		}
-//
-//	}
+	public void setText(int id, CharSequence text) {
+		View v = findViewById(id);
+		if (null != v && v instanceof TextView) {
+			((TextView) v).setText(text);
+		}
+
+	}
 
 	// method for setting drawable for the images
-//	public void setImageDrawable(int id, Drawable drawable) {
-//
-//		View v = findViewById(id);
-//		if (null != v && v instanceof ImageView) {
-//			((ImageView) v).setImageDrawable(drawable);
-//		}
-//
-//	}
+	public void setImageDrawable(int id, Drawable drawable) {
+
+		View v = findViewById(id);
+		if (null != v && v instanceof ImageView) {
+			((ImageView) v).setImageDrawable(drawable);
+		}
+
+	}
 
 	// method for setting images by resource id
-//	public void setImageResource(int id, int image_resource_id) {
-//
-//		View v = findViewById(id);
-//		if (null != v && v instanceof ImageView) {
-//			((ImageView) v).setImageResource(image_resource_id);
-//		}
-//
-//	}
+	public void setImageResource(int id, int image_resource_id) {
+
+		View v = findViewById(id);
+		if (null != v && v instanceof ImageView) {
+			((ImageView) v).setImageResource(image_resource_id);
+		}
+
+	}
 
 }
