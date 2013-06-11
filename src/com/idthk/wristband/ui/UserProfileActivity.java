e46ff9656bcabb22cf59f3d6652ca36b83deab2f
James Kong <fishkingsin@gmail.com>
2013-06-11 09:17:05 +0800

    update for preference screen

--- src/com/idthk/wristband/ui/UserProfileActivity.java
+++ src/com/idthk/wristband/ui/UserProfileActivity.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.util.Calendar;
 
 import com.idthk.wristband.ui.R;
+import com.idthk.wristband.ui.FragmentPreferences.PrefsFragment;
+import com.idthk.wristband.ui.numberpickerpreference.NumberPickerPreference;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -23,6 +25,9 @@ import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.provider.MediaStore;
 import android.provider.MediaStore.Images.ImageColumns;
 import android.provider.MediaStore.Images.Media;
@@ -42,128 +47,131 @@ public class UserProfileActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.user_profile);
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+		
+		getFragmentManager().beginTransaction().replace(android.R.id.content,
+                new UserPrefsFragment()).commit();
 		mContext = this;
-		((Button) findViewById(R.id.btn_settings_done))
-				.setOnClickListener(new OnClickListener() {
-					public void onClick(View m) {
-						Log.v(TAG, "onClick");
-						finish();
-					}
-				});
-		;
-
-		((RelativeLayout) findViewById(R.id.gender_layout))
-				.setOnClickListener(new OnClickListener() {
-					public void onClick(View m) {
-						Log.v(TAG, "onClick");
-
-					}
-				});
-		((RelativeLayout) findViewById(R.id.birthday_layout))
-				.setOnClickListener(new OnClickListener() {
-					public void onClick(View m) {
-						Log.v(TAG, "onClick");
-
-					}
-				});
-		((RelativeLayout) findViewById(R.id.unit_layout))
-				.setOnClickListener(new OnClickListener() {
-					public void onClick(View m) {
-						Log.v(TAG, "onClick");
-
-					}
-				});
-		((RelativeLayout) findViewById(R.id.height_layout))
-				.setOnClickListener(new OnClickListener() {
-					public void onClick(View m) {
-						Log.v(TAG, "onClick");
-
-					}
-				});
-		((RelativeLayout) findViewById(R.id.weight_layout))
-				.setOnClickListener(new OnClickListener() {
-					public void onClick(View m) {
-						Log.v(TAG, "onClick");
-
-					}
-				});
-		ImageView img = ((ImageView) findViewById(R.id.user_profile_pic));
-		
-		SharedPreferences prefs = getSharedPreferences(getString(R.string.pref_name), 0);
-        String path = prefs.getString("profile_pic", "");
-        Log.v(TAG,"profile path : "+path);
-        if(path!="")
-        {
-        	Bitmap myBitmap = Utilities.decodeFile(new File(path),this);
-        	img.setImageBitmap(myBitmap);
-        }
-		
-		img.setOnClickListener(new OnClickListener() {
-			public void onClick(View m) {
-				// Log.v(TAG,"onClick");
-				// Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
-				// photoPickerIntent.setType("image/*");
-				// startActivityForResult(photoPickerIntent,
-				// Main.IMAGE_GALLERY_REQUEST);
-				showDialog();
-			}
-		});
+//		((Button) findViewById(R.id.btn_settings_done))
+//				.setOnClickListener(new OnClickListener() {
+//					public void onClick(View m) {
+//						Log.v(TAG, "onClick");
+//						finish();
+//					}
+//				});
+//		;
+//
+//		((RelativeLayout) findViewById(R.id.gender_layout))
+//				.setOnClickListener(new OnClickListener() {
+//					public void onClick(View m) {
+//						Log.v(TAG, "onClick");
+//
+//					}
+//				});
+//		((RelativeLayout) findViewById(R.id.birthday_layout))
+//				.setOnClickListener(new OnClickListener() {
+//					public void onClick(View m) {
+//						Log.v(TAG, "onClick");
+//
+//					}
+//				});
+//		((RelativeLayout) findViewById(R.id.unit_layout))
+//				.setOnClickListener(new OnClickListener() {
+//					public void onClick(View m) {
+//						Log.v(TAG, "onClick");
+//
+//					}
+//				});
+//		((RelativeLayout) findViewById(R.id.height_layout))
+//				.setOnClickListener(new OnClickListener() {
+//					public void onClick(View m) {
+//						Log.v(TAG, "onClick");
+//
+//					}
+//				});
+//		((RelativeLayout) findViewById(R.id.weight_layout))
+//				.setOnClickListener(new OnClickListener() {
+//					public void onClick(View m) {
+//						Log.v(TAG, "onClick");
+//
+//					}
+//				});
+//		ImageView img = ((ImageView) findViewById(R.id.user_profile_pic));
+//		
+//		SharedPreferences prefs = getSharedPreferences(getString(R.string.pref_name), 0);
+//        String path = prefs.getString(getString(R.string.key_profile_pic), "");
+//        Log.v(TAG,"profile path : "+path);
+//        if(path!="")
+//        {
+//        	Bitmap myBitmap = Utilities.decodeFile(new File(path),this);
+//        	img.setImageBitmap(myBitmap);
+//        }
+//		
+//		img.setOnClickListener(new OnClickListener() {
+//			public void onClick(View m) {
+//				// Log.v(TAG,"onClick");
+//				// Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+//				// photoPickerIntent.setType("image/*");
+//				// startActivityForResult(photoPickerIntent,
+//				// Main.IMAGE_GALLERY_REQUEST);
+//				showDialog();
+//			}
+//		});
 	}
 
-	private void showDialog() {
-		final String[] list = {
-				(String) getResources().getText(R.string.take_picture),
-				(String) getResources().getText(R.string.choose_from_library) };// ,
-		// (String) getResources().getText(R.string.restore_profile_picture)};
-
-		AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-
-		DialogInterface.OnClickListener listItemOnClick = new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int position) {
-				Intent intent;
-				switch (position) {
-				case 0:
-					intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-					intent.putExtra(MediaStore.EXTRA_OUTPUT,
-							Uri.fromFile(getTempFile(mContext)));
-					startActivityForResult(intent, Main.TAKE_PHOTO_CODE);
-					break;
-				case 1:
-					intent = new Intent(
-							Intent.ACTION_PICK,
-							android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-					startActivityForResult(intent, Main.SELECT_IMAGE_CODE);
-					break;
-				case 2:
-					// SharedPreferences settings =
-					// getSharedPreferences("Preferences", 0);
-					// SharedPreferences.Editor editor = settings.edit();
-					// if (mBackgroundProfile == 0){
-					// editor.putString("DEFAULT_BACKGROUND", "");
-					// mChangeBackgroundImageView.setImageDrawable(getResources().getDrawable(R.drawable.default_background));
-					// } else {
-					// editor.putString("USER_AVATAR", "null");
-					// mChangeProfileImageView.setImageDrawable(getResources().getDrawable(R.drawable.community_default_icon));
-					// mDefaultAvatar = "1";
-					// }
-					// editor.commit();
-
-					break;
-				}
-
-			}
-		};
-
-		DialogInterface.OnClickListener cancelBtnOnClick = new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int position) {
-
-			}
-		};
-
-		alertDialog.setItems(list, listItemOnClick);
-		alertDialog.setNeutralButton("Cancel", cancelBtnOnClick);
-		alertDialog.show();
-	}
+//	private void showDialog() {
+//		final String[] list = {
+//				(String) getResources().getText(R.string.take_picture),
+//				(String) getResources().getText(R.string.choose_from_library) };// ,
+//		// (String) getResources().getText(R.string.restore_profile_picture)};
+//
+//		AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+//
+//		DialogInterface.OnClickListener listItemOnClick = new DialogInterface.OnClickListener() {
+//			public void onClick(DialogInterface dialog, int position) {
+//				Intent intent;
+//				switch (position) {
+//				case 0:
+//					intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//					intent.putExtra(MediaStore.EXTRA_OUTPUT,
+//							Uri.fromFile(getTempFile(mContext)));
+//					startActivityForResult(intent, Main.TAKE_PHOTO_CODE);
+//					break;
+//				case 1:
+//					intent = new Intent(
+//							Intent.ACTION_PICK,
+//							android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+//					startActivityForResult(intent, Main.SELECT_IMAGE_CODE);
+//					break;
+//				case 2:
+//					// SharedPreferences settings =
+//					// getSharedPreferences("Preferences", 0);
+//					// SharedPreferences.Editor editor = settings.edit();
+//					// if (mBackgroundProfile == 0){
+//					// editor.putString("DEFAULT_BACKGROUND", "");
+//					// mChangeBackgroundImageView.setImageDrawable(getResources().getDrawable(R.drawable.default_background));
+//					// } else {
+//					// editor.putString("USER_AVATAR", "null");
+//					// mChangeProfileImageView.setImageDrawable(getResources().getDrawable(R.drawable.community_default_icon));
+//					// mDefaultAvatar = "1";
+//					// }
+//					// editor.commit();
+//
+//					break;
+//				}
+//
+//			}
+//		};
+//
+//		DialogInterface.OnClickListener cancelBtnOnClick = new DialogInterface.OnClickListener() {
+//			public void onClick(DialogInterface dialog, int position) {
+//
+//			}
+//		};
+//
+//		alertDialog.setItems(list, listItemOnClick);
+//		alertDialog.setNeutralButton("Cancel", cancelBtnOnClick);
+//		alertDialog.show();
+//	}
 
 //	protected void onActivityResult(int requestCode, int resultcode,
 //			Intent intent) {
@@ -199,7 +207,7 @@ public class UserProfileActivity extends Activity {
 //			}
 //		}
 //	}
-	@Override
+	/*@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		Utilities.getLog("resultCode", resultCode+"");
 		
@@ -238,7 +246,7 @@ public class UserProfileActivity extends Activity {
 //					}
 				    captureBmp.compress(Bitmap.CompressFormat.PNG, 100, fos);
 				    ((ImageView) findViewById(R.id.user_profile_pic)).setImageBitmap(captureBmp);
-				    editor.putString("profile_pic",file.getAbsolutePath());
+				    editor.putString(getString(R.string.key_profile_pic),file.getAbsolutePath());
 				    
 		    		editor.commit();
 				    fos.close();
@@ -282,7 +290,7 @@ public class UserProfileActivity extends Activity {
 					selectedImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
 					((ImageView) findViewById(R.id.user_profile_pic)).setImageBitmap(selectedImage);
 					
-					 editor.putString("profile_pic",filePath);
+					 editor.putString(getString(R.string.key_profile_pic),filePath);
 			    		editor.commit();
 					
 				    fos.close();
@@ -327,6 +335,50 @@ public class UserProfileActivity extends Activity {
 		int seconds = c.get(Calendar.SECOND);
 
 		return new File(path, "image.png");
-	}
+	}*/
 
+	
+	public static class UserPrefsFragment extends PreferenceFragment 
+	implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.userprofile_preferences);
+            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this.getActivity());
+            sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+            Preference pref = findPreference(getString(R.string.key_user_name));
+        	pref.setSummary(sharedPreferences.getString(getString(R.string.key_user_name), getString(R.string.default_user_name)));
+        	pref = findPreference("prefUserGender");
+        	pref.setSummary(sharedPreferences.getString("prefUserGender", getString(R.string.default_user_gender)));
+        }
+        @Override
+    	public void onDestroyView()
+    	{
+        	SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this.getActivity());
+            sharedPreferences.unregisterOnSharedPreferenceChangeListener(this);
+    		super.onDestroyView();
+    	}
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            // handle the preference change here'
+        	Log.v(TAG,"key : "+sharedPreferences.toString() +" "+key +" "+sharedPreferences.getAll().toString());
+        	Preference pref = findPreference(key);
+        	if (key.equals("prefUserGender")) {
+        		
+        		pref.setSummary(sharedPreferences.getString(key, getString(R.string.default_user_gender)));
+            } else if (key.equals(getString(R.string.key_user_name))) {
+            	pref.setSummary(sharedPreferences.getString(key, getString(R.string.default_user_name)));
+            }else if (key.equals("prefDateOfBirth")) {
+            	pref.setSummary(sharedPreferences.getString(key, "1991.01.01"));
+            }
+            else if (key.equals("prefHeight")) {
+            	pref.setSummary(sharedPreferences.getInt(key, 178));
+            }else if (key.equals("prefWeight")) {
+            	pref.setSummary(sharedPreferences.getInt(key, 50));
+            }
+        }
+    }
 }
